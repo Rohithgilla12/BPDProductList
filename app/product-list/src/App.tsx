e3ce11dde4226ec0028ea1224f4b24c4d32fd405
Rohithgilla12 <gillarohith1@gmail.com>
2020-12-14 14:36:08 +0530

    Minor SEO stuff

--- app/product-list/src/App.tsx
+++ app/product-list/src/App.tsx
@@ -7,12 +7,14 @@ import {
 	Route,
 } from "react-router-dom";
 import { ProductList } from './features/product/ProductList';
+import { Typography } from '@material-ui/core';
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
 				<header className="App-header">
+					<Typography variant="h3">Balaji Pharma Distributors Product List</Typography>
 					<Switch>
 						<Route path="/products">
 							<ProductList />
